a368e0934286ec2873d79679d1c84bc67c32bef1
mfiro <mfiro@protonmail.com>
2022-02-08 23:23:03 +0100

    Fix a bug in human_readable_clue Add infinite loop of the game

--- game.py
+++ game.py
@@ -10,12 +10,15 @@ def human_readable_clue(analyse_result):
     """Convert correct, misplaced number to a human readable clue
     
     There are 9 possible tuples:
-    (0, 1), (0, 2), (0, 3)
+
+    (0,0), (0, 1), (0, 2), (0, 3)
     (1, 0), (1, 1), (1, 2)
     (2, 0), (2, 1)
     (3, 0) -> this one is caught earlier, no need to implement it.
     """
-    if analyse_result == (0, 1):
+    if analyse_result == (0, 0):
+        return "Nothing is correct"
+    elif analyse_result == (0, 1):
         return "One digit is correct but wrongly placed"
     elif analyse_result == (0, 2):
         return "Two digits are correct but wrongly placed"
@@ -33,25 +36,13 @@ def human_readable_clue(analyse_result):
         return "Two digits are correct and well placed"
     elif analyse_result == (2, 1):
         return "All digits are correct but one of them is wrongly placed"
-   
     else:
         return "N/A"
-    
 
-if __name__ == "__main__":
-    init_message = """
-    Welcome!
-    This puzzle includes a box which is locked by a three digit combination lock!
-    You have 5 attempts to find the solution.
-    After each attempt, you'll get a clue which helps you.
 
-    Let's begin
-    """
-    print(init_message)
-    print(f"{'-'*100}")
+def game():
 
     solution = generate_puzzle()
-
     max_attempt = 5
     n_attempt = 1
 
@@ -70,6 +61,29 @@ if __name__ == "__main__":
 
     else:
         print("Failed!")
+
+
+if __name__ == "__main__":
+    init_message = """
+    Welcome!
+    This puzzle includes a box which is locked by a three digit combination lock!
+    You have 5 attempts to find the solution.
+    After each attempt, you'll get a clue which helps you.
+
+    Let's begin
+    """
+    print(init_message)
+    print(f"{'-'*100}")
+
+    solution = generate_puzzle()
+
+    play_game = True
+    while play_game:
+        game()
+        response = input("Play again? [Y/n]")
+        if response.lower() != 'y':
+            play_game = False
+        
 
 
 
